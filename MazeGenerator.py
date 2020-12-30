ccf71f25d05275d4b1f368a6fade838d2e5a447b
Steven Liu <sliu123@syr.edu>
2020-12-30 16:51:22 -0500

    Worked on GUI

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -1,19 +1,36 @@
-from tkinter import *
+from graphics import *
 
 rows = 10
 cols = 10
 
 def main():
-    mainwindow = Tk()
-    mainwindow.geometry(str(rows*40)+"x"+str(cols*40))
-    mainwindow.mainloop()
+    mainwindow = GraphWin("Maze Generator", cols*40, rows*40)
+    mainwindow.setBackground("White")
 
     NodeList = []
-    
+
     for i in range (rows):
         for j in range (cols):
             NewNode = Node(i,j)
             NodeList.append(NewNode)
+
+    for i in NodeList:
+        if i.top:
+            topline = Line(Point(i.row*40, i.col*40), Point(i.row*40+40, i.col*40))
+            topline.draw(mainwindow)
+        if i.left:
+            topline = Line(Point(i.row*40, i.col*40), Point(i.row*40, i.col*40+40))
+            topline.draw(mainwindow)
+        if i.right:
+            topline = Line(Point(i.row*40+40, i.col*40), Point(i.row*40+40, i.col*40+40))
+            topline.draw(mainwindow)
+        if i.bot:
+            topline = Line(Point(i.row*40, i.col*40+40), Point(i.row*40+40, i.col*40+40))
+            topline.draw(mainwindow)
+
+            
+    print("Test")
+
 
 
 
@@ -21,6 +38,10 @@ class Node:
     def __init__(self, row, col):
         self.row = row
         self.col = col
+        self.top = True    
+        self.left = False
+        self.right = False
+        self.bot = False
     
 
 

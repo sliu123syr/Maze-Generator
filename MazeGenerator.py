6181f5145e51c99378ce64aa90de7c420fbf88cc
Steven Liu <sliu123@syr.edu>
2021-01-02 19:36:08 -0500

    Update MazeGenerator.py
    Some bugfixing.

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -12,33 +12,33 @@ def main():
 
     for i in range (rows):
         for j in range (cols):
-            newNode = Node(i,j)
+            newNode = Node(i,j,len(nodeList)+1)
             nodeList.append(newNode)
 
     curNode = nodeList[0]
-
     draw(mainwindow)
 
 
 def draw(mainwindow):
     for i in nodeList:
         if i.top:
-            topline = Line(Point(i.row*40, i.col*40), Point(i.row*40+40, i.col*40))
+            topline = Line(Point(i.col*40, i.row*40), Point(i.col*40+40, i.row*40))
             topline.draw(mainwindow)
         if i.left:
-            topline = Line(Point(i.row*40, i.col*40), Point(i.row*40, i.col*40+40))
+            topline = Line(Point(i.col*40, i.row*40), Point(i.col*40, i.row*40+40))
             topline.draw(mainwindow)
         if i.right:
-            topline = Line(Point(i.row*40+40, i.col*40), Point(i.row*40+40, i.col*40+40))
+            topline = Line(Point(i.col*40+40, i.row*40), Point(i.col*40+40, i.row*40+40))
             topline.draw(mainwindow)
         if i.bot:
-            topline = Line(Point(i.row*40, i.col*40+40), Point(i.row*40+40, i.col*40+40))
+            topline = Line(Point(i.col*40, i.row*40+40), Point(i.col*40+40, i.row*40+40))
             topline.draw(mainwindow)
 
 class Node:
-    def __init__(self, row, col):
+    def __init__(self, row, col, index):
         self.row = row
         self.col = col
+        self.index = index
         self.top = True    
         self.left = True
         self.right = True
@@ -64,25 +64,24 @@ class Node:
         return self.visited
 
     def randomneighbor(self):
-        index = 2*self.row + self.col
         while True:
             num = random.randint(1,4)
-            if (num == 1) and index > self.col and nodeList[index - self.col].getvisited() == False:
+            if (num == 1) and self.index > self.col and nodeList[self.index - self.col].getvisited() == False:
                 self.top = False
-                nodeList[index - self.col].setbot(False)
-                return nodeList[index - self.col]
-            if num == 2 and index%self.col != 1 and nodeList[index - 1].getvisited() == False:
+                nodeList[self.index - self.col].setbot(False)
+                return nodeList[self.index - self.col]
+            if num == 2 and self.index%self.col != 1 and nodeList[self.index - 1].getvisited() == False:
                 self.left = False
-                nodeList[index - 1].setright(False)
-                return nodeList[index - 1]
-            if num == 1 and index%self.col != 0 and nodeList[index + 1].getvisited() == False:
+                nodeList[self.index - 1].setright(False)
+                return nodeList[self.index - 1]
+            if num == 1 and self.index%self.col != 0 and nodeList[self.index + 1].getvisited() == False:
                 self.right = False
-                nodeList[index + 1].setleft(False)
-                return nodeList[index + 1]
-            if num == 1 and index+self.col <= self.row*self.col and nodeList[index + self.col].getvisited() == False:
+                nodeList[self.index + 1].setleft(False)
+                return nodeList[self.index + 1]
+            if num == 1 and self.index+self.col <= self.row*self.col and nodeList[self.index + self.col].getvisited() == False:
                 self.bot = False
-                nodeList[index + self.col].settop(False)
-                return nodeList[index + self.col]
+                nodeList[self.index + self.col].settop(False)
+                return nodeList[self.index + self.col]
             
         
 
